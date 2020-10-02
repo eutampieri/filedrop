a7bfac4f6d5d10b853fc6f75a3841de5fc9c85f3
Eugenio Tampieri <eugenio@eutampieri.eu>
2020-10-02 17:37:20 +0200

    Revert "Do not attempt any conversion"
    This reverts commit 6a08185624afbc2bd858ee8c29896d59f0868027.

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,3 +1,6 @@
+use image::{GenericImageView, ImageDecoder};
+use std::io::Cursor;
+
 pub enum Format {
     Jpeg,
     Webp,
@@ -37,20 +40,9 @@ impl Format {
             Self::Ico => "image/vnd.microsoft.icon",
         }
     }
-    pub fn get_ext(&self) -> &'static str {
-        match self {
-            Self::Jpeg => "jpeg",
-            Self::Webp => "webp",
-            Self::Gif => "gif",
-            Self::Bmp => "bmp",
-            Self::Tiff => "tiff",
-            Self::Png => "png",
-            Self::Ico => "ico",
-        }
-    }
 }
 
-pub fn decode_image(img: &str) -> Result<(Vec<u8>, Format), &'static str> {
+pub fn decode_image(img: &str) -> Result<Vec<u8>, &'static str> {
     let image = base64::decode(img).map_err(|_| "Cannot decode image from b64")?;
     if image.len() > 5_000_000 {
         return Err("File is too big");
@@ -61,8 +53,68 @@ pub fn decode_image(img: &str) -> Result<(Vec<u8>, Format), &'static str> {
     if mime.mime.split("/").nth(0).unwrap() != "image" {
         return Err("The file is not an image");
     }
-    Ok((
-        image,
-        Format::from(mime.mime.split("/").nth(1).unwrap().to_owned()),
-    ))
+    let decoded = image::load_from_memory(&image).map_err(|_| "Cannot decode the image")?;
+    let dim = decoded.dimensions();
+    libwebp::WebPEncodeLosslessRGBA(&decoded.into_rgba(), dim.0, dim.1, 8)
+        .map_err(|_| "Cannot encode to WebP")
+        .map(|x| Vec::from(&*x)) // TODO Fix this alloc
+}
+
+pub fn encode_img(webp: &[u8], format: &Format) -> Result<Vec<u8>, &'static str> {
+    let (width, height, img_data) =
+        libwebp::WebPDecodeRGBA(webp).map_err(|_| "Cannot decode WebP")?;
+    let img_color_type = image::ColorType::Rgba8;
+    let dim = (width, height);
+    match format {
+        Format::Bmp => {
+            let mut output = Vec::<u8>::new();
+            let mut encoder = image::bmp::BmpEncoder::new(&mut output);
+            encoder
+                .encode(&img_data, dim.0, dim.1, img_color_type)
+                .map_err(|_| "Cannot encode image")?;
+            Ok(output)
+        }
+        Format::Gif => {
+            let mut output = Vec::<u8>::new();
+            let mut encoder = image::gif::GifEncoder::new(&mut output);
+            encoder
+                .encode(&img_data, dim.0, dim.1, img_color_type)
+                .map_err(|_| "Cannot encode image")?;
+            drop(encoder);
+            Ok(output)
+        }
+        Format::Ico => {
+            let mut output = Vec::<u8>::new();
+            let encoder = image::ico::IcoEncoder::new(&mut output);
+            encoder
+                .encode(&img_data, dim.0, dim.1, img_color_type)
+                .map_err(|_| "Cannot encode image")?;
+            Ok(output)
+        }
+        Format::Jpeg => {
+            let mut output = Vec::<u8>::new();
+            let mut encoder = image::jpeg::JpegEncoder::new(&mut output);
+            encoder
+                .encode(&img_data, dim.0, dim.1, img_color_type)
+                .map_err(|_| "Cannot encode image")?;
+            Ok(output)
+        }
+        Format::Png => {
+            let mut output = Vec::<u8>::new();
+            let encoder = image::png::PngEncoder::new(&mut output);
+            encoder
+                .encode(&img_data, dim.0, dim.1, img_color_type)
+                .map_err(|_| "Cannot encode image")?;
+            Ok(output)
+        }
+        Format::Tiff => {
+            let mut output = Vec::<u8>::new();
+            let encoder = image::tiff::TiffEncoder::new(Cursor::new(&mut output));
+            encoder
+                .encode(&img_data, dim.0, dim.1, img_color_type)
+                .map_err(|_| "Cannot encode image")?;
+            Ok(output)
+        }
+        Format::Webp => Ok(Vec::from(webp)),
+    }
 }
--- src/routes.rs
+++ src/routes.rs
@@ -9,15 +9,17 @@ pub struct Upload {
 
 #[get("/{name}.{ext}")]
 pub async fn get_file(web::Path((name, ext)): web::Path<(String, String)>) -> Result<HttpResponse> {
-    let fmt: conversion::Format = ext.clone().into();
-    if !std::path::Path::new(&format!("pool/{}.{}", name, ext)).exists() {
+    let fmt: conversion::Format = ext.into();
+    if !std::path::Path::new(&format!("pool/{}.webp", name)).exists() {
         Ok(HttpResponse::NotFound().finish())
     } else {
-        let img = std::fs::read(&format!("pool/{}.{}", name, ext))
-            .map_err(|_| HttpResponse::NotFound())?;
+        let webp =
+            std::fs::read(&format!("pool/{}.webp", name)).map_err(|_| HttpResponse::NotFound())?;
+        let output = conversion::encode_img(&webp, &fmt)
+            .map_err(|x| HttpResponse::InternalServerError().body(x))?;
         Ok(HttpResponse::Ok()
             .content_type(fmt.get_content_type())
-            .body(img))
+            .body(output))
     }
 }
 
@@ -25,11 +27,11 @@ pub async fn get_file(web::Path((name, ext)): web::Path<(String, String)>) -> Re
 pub async fn save_file(form: web::Form<Upload>) -> Result<String> {
     let image =
         conversion::decode_image(&form.img).map_err(|x| HttpResponse::BadRequest().body(x))?;
-    let hash = file_encoding::hash_file(&image.0);
+    let hash = file_encoding::hash_file(&image);
     if !std::path::Path::new(&format!("pool/{}", hash)).exists() {
         // Save file
-        std::fs::write(format!("pool/{}.{}", hash, image.1.get_ext()), image.0)
+        std::fs::write(format!("pool/{}.webp", hash), image)
             .map_err(|_| HttpResponse::InsufficientStorage())?;
     }
-    Ok(format!("https://i.shlnk.eu/{}.{}", hash, image.1.get_ext()))
+    Ok(format!("https://i.shlnk.eu/{}", hash))
 }
