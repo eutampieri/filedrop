0b55aab1c497fb16c31a901c6be12855568bf20f
Michael Chelli <michaelchelli00@gmail.com>
2020-10-03 11:44:43 +0200

    added rustfmt, alphabet, blake3

--- src/file_encoding.rs
+++ src/file_encoding.rs
@@ -1,5 +1,14 @@
+
+
+const ALPHABET: [char;62] = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+const MOD: u64 = 56800235584; //62**6
+
+fn num_to_string(n: u64){
+	
+}
+
 pub fn hash_file(file: &[u8]) -> String {
-    "a".to_owned()
+    blake3::hash(file).to_string();
 }
 
 #[cfg(test)]
