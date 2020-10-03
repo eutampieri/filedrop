6595fa5751022172736da771729094283ac4f8c3
Eugenio Tampieri <eugenio@eutampieri.eu>
2020-10-03 11:50:11 +0200

    Hasher is now a struct

--- src/file_encoding.rs
+++ src/file_encoding.rs
@@ -1,30 +1,39 @@
-pub fn hash_file(file: &[u8]) -> String {
-    "a".to_owned()
+#[derive(Default)]
+pub struct FileHasher;
+impl FileHasher {
+    pub fn hash_file(&mut self, file: &[u8]) -> String {
+        "a".to_owned()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
     fn test_same_output() {
+        let hasher = FileHasher::default();
         assert_eq!(
-            hash_file(&[0, 7, 8, 2, 6, 4, 59, 38]),
-            hash_file(&[0, 7, 8, 2, 6, 4, 59, 38])
+            hasher.hash_file(&[0, 7, 8, 2, 6, 4, 59, 38]),
+            hasher.hash_file(&[0, 7, 8, 2, 6, 4, 59, 38])
         );
     }
 
     #[test]
     fn test_const_len() {
+        let hasher = FileHasher::default();
         assert_eq!(
-            hash_file(&[0, 7, 8, 2, 6, 4, 59, 38, 78, 159, 48]).len(),
-            hash_file(&[0, 7, 8, 2, 6, 4, 59, 38]).len()
+            hasher
+                .hash_file(&[0, 7, 8, 2, 6, 4, 59, 38, 78, 159, 48])
+                .len(),
+            hasher.hash_file(&[0, 7, 8, 2, 6, 4, 59, 38]).len()
         );
     }
     #[test]
     fn test_allowed_chars() {
-        assert!(hash_file(&[0, 7, 8, 2, 6, 4, 59, 38, 78, 159, 48])
+        let hasher = FileHasher::default();
+        assert!(hasher
+            .hash_file(&[0, 7, 8, 2, 6, 4, 59, 38, 78, 159, 48])
             .as_str()
             .chars()
             .into_iter()
@@ -32,7 +41,10 @@ mod tests {
     }
     #[test]
     fn test_length() {
-        let len = hash_file(&[0, 7, 8, 2, 6, 4, 59, 38, 78, 159, 48]).len();
+        let hasher = FileHasher::default();
+        let len = hasher
+            .hash_file(&[0, 7, 8, 2, 6, 4, 59, 38, 78, 159, 48])
+            .len();
         assert!(len <= 6);
     }
 }
